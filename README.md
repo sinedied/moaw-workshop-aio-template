e64560f4e1edfafdafb316943cbb9aa11167f4b8
sinedied <noda@free.fr>
2024-02-27 13:25:59 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# ⚙️ moaw-workshop-aio-template
+
+This repository contains an all-in-one template for creating a [MOAW](https://moaw.dev) workshop.
+
+This template includes the following:
+- Built-in MOAW workshop documentation, located in the `docs` folder.
+- A GitHub Actions workflow for automatically creating the workshop template in the `main` branch (the reference branch used is the `base` branch).
+- A GitHub Actions workflow for automatically creating packages for the workshop (for intermediate steps, solution, etc).
+
+## How to use this template
+
+### Initial setup
+
+
+### Creating your workshop
+
+
+## Contributing
+
+This project welcomes contributions and suggestions. Most contributions require you to agree to a
+Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
+the rights to use your contribution. For details, visit https://cla.opensource.microsoft.com.
+
+When you submit a pull request, a CLA bot will automatically determine whether you need to provide
+a CLA and decorate the PR appropriately (e.g., status check, comment). Simply follow the instructions
+provided by the bot. You will only need to do this once across all repos using our CLA.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
+contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+## Trademarks
+
+This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft
+trademarks or logos is subject to and must follow
+[Microsoft's Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
+Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
+Any use of third-party trademarks or logos are subject to those third-party's policies.
---
+++ scripts/repo/create-github-template.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+##############################################################################
+# Usage: ./create-github-template.sh [--local]
+# Creates the project template and push it to GitHub.
+##############################################################################
+
+##############################################################################
+# TODO: Update the GitHub repository details
+##############################################################################
+
+GITHUB_ORG=sinedied
+GITHUB_REPO=moaw-workshop-aio-template
+
+##############################################################################
+
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ../..
+
+TEMPLATE_HOME=/tmp/${GITHUB_REPO}
+GH_USER=$(git config user.name)
+TEMPLATE_REPO=https://$GH_USER:$GITHUB_TOKEN@github.com/${GITHUB_ORG}/${GITHUB_REPO}.git
+
+echo "Preparing GitHub project template..."
+echo "(temp folder: $TEMPLATE_HOME)"
+rm -rf "$TEMPLATE_HOME"
+
+# Clone the template repo and start from the base branch to keep
+# the contributors history in the main branch we'll overwrite
+git clone "$TEMPLATE_REPO" "$TEMPLATE_HOME"
+pushd "$TEMPLATE_HOME"
+git reset --hard origin/base
+popd
+
+find . -type d -not -path '*node_modules*' -not -path '*.git/*' -not -path '*/dist' -not -path '*dist/*' -exec mkdir -p '{}' "$TEMPLATE_HOME/{}" ';'
+find . -type f -not -path '*node_modules*' -not -path '*.git/*' -not -path '*dist/*' -not -path '*/.DS_Store' -exec cp -r '{}' "$TEMPLATE_HOME/{}" ';'
+cd "$TEMPLATE_HOME"
+
+##############################################################################
+# TODO: Prepare the project template
+##############################################################################
+
+# Remove unnecessary files
+rm -rf node_modules
+rm -rf .github
+rm -rf TODO
+rm -rf package-lock.json
+rm -rf scripts/repo
+rm -rf docs
+rm -rf .azure
+rm -rf .env
+rm -rf ./*.env
+
+# Prepare files
+echo -e "console.log('hello world!')" > index.js
+
+##############################################################################
+
+# Prepare the commit
+git add .
+git commit -m "chore: prepare project template"
+
+if [[ ${1-} == "--local" ]]; then
+  echo "Local mode: skipping GitHub push."
+  open "$TEMPLATE_HOME"
+else
+  # Update git repo
+  git push -u origin main --force
+
+  rm -rf "$TEMPLATE_HOME"
+fi
+
+echo "Successfully updated project template."
